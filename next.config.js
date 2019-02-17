5056589a4f056381c166b7adcc8b89fd4619b975
Ben Wilcox <reccanti@gmail.com>
2019-02-17 00:33:52 -0500

    Update docs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 const process = require("process");
 
 module.exports = {
+  exportPathMap() {
+    return {
+      '/': { page: '/' },
+      '/demos/01-basic-triangle': { page: '/demos/01-basic-triangle' }
+    }
+  },
   assetPrefix: process.env.NODE_ENV === "production" ? "https://reccanti.github.io/webgl-demos" : "",
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
