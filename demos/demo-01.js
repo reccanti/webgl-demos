544693db3404d8eb4e8a9833cc5bbdea8c59f32e
Ben Wilcox <reccanti@gmail.com>
2019-02-16 21:16:21 -0500

    Try to render site again

--- demos/demo-01.js
+++ demos/demo-01.js
@@ -5,7 +5,7 @@ const vertShaderSource = glsl`
 
     void main() {
         gl_Position = a_position;
-    };
+    }
 `;
 
 const fragShaderSource = glsl`
@@ -13,7 +13,7 @@ const fragShaderSource = glsl`
 
     void main() {
         gl_FragColor = vec4(1, 0, 0.5, 1);
-    };
+    }
 `;
 
 function createShader(gl, type, source) {
