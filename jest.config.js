905c5010213d8770bc615a21d2bd284afb8b9819
Ben Wilcox <reccanti@gmail.com>
2019-02-27 00:17:02 -0500

    Add support for enzyme

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,16 @@
+const TEST_REGEX = '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|js?|tsx?|ts?)$'
+
 module.exports = {
+    "setupFiles": ["<rootDir>/enzymeConfig.js"],
+    "testRegex": TEST_REGEX,
     "transform": {
         "^.+\\.tsx?$": "ts-jest"
     },
+    "testPathIgnorePatterns": ["<rootDir>/.next/", "<rootDir>/node_modules/"],
     "moduleFileExtensions": [
         "ts",
         "tsx",
         "js",
-        "jsx",
-        "json",
-        "node"
+        "jsx"
     ],
 }
