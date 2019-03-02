3cef7006cd781336c5b1eafa9c43ffb6327d8df6
Ben Wilcox <reccanti@gmail.com>
2019-03-02 02:10:53 -0500

    Allow ValueSetter to update values

--- components/common/__test__/valueSetterHoc.test.tsx
+++ components/common/__test__/valueSetterHoc.test.tsx
@@ -2,12 +2,14 @@ import * as React from 'react'
 import { shallow } from 'enzyme';
 import makeValueSetter from '../valueSetterHoc';
 
+type TestConfigShape = void;
+type TestValueShape = {
+    foo: string
+}
+
 describe("value-setter HOC", () => {
     test("passes down an initial set of values", () => {
-        const ValueSetter = makeValueSetter<void, { foo: string }>(() => ({ foo: "bar" }), { foo: "baz" });
-        // const renderer = createRenderer();
-        // renderer.render(<ValueSetter>{values => (<div>{JSON.stringify(values)}</div>)}</ValueSetter>)
-        // console.log(renderer.getRenderOutput);
+        const ValueSetter = makeValueSetter<TestConfigShape, TestValueShape>(() => ({ foo: "bar" }), { foo: "baz" });
         shallow(
             <ValueSetter>
                 {values => {
@@ -16,5 +18,18 @@ describe("value-setter HOC", () => {
                 }}
             </ValueSetter>
         );
+    })
+    test("updates values when the `init` function is called", () => {
+        const MockComponent = jest.fn();
+        const ValueSetter = makeValueSetter<TestConfigShape, TestValueShape>(() => ({ foo: "bar" }), { foo: "baz" });
+        const wrapper = shallow(
+            <ValueSetter>
+                {values => {
+                    return <MockComponent {...values} />
+                }}
+            </ValueSetter>
+        )
+        wrapper.find(MockComponent).props().init()
+        expect(wrapper.find(MockComponent).props().foo).toBe("bar");
     })
 })
--- components/common/valueSetterHoc.tsx
+++ components/common/valueSetterHoc.tsx
@@ -1,17 +1,39 @@
+/**
+ * A higher-order component used to store arbitrary values that can
+ * be set in a child component. Useful for something like a canvas
+ * that requires initialization, but may need to be updated by a 
+ * component at the same level or higher
+ */
 import * as React from 'react';
 
 type Props<S, T> = {
     children: (values: T & { init: (config: S) => void }) => React.ReactNode
 }
+type State<T> = T
 
-export default function makeValueSetter<S, T>(_initializer: (config: S) => T, initialValue: T): React.FunctionComponent<Props<S, T>> {
+export default function makeValueSetter<S, T>(initializer: (config: S) => T, initialValue: T): React.ComponentClass<Props<S, T>, State<T>> {
 
-    return (props: Props<S, T>) => (
-        <React.Fragment>
-            {props.children({
-                init: () => { },
-                ...initialValue
-            })}
-        </React.Fragment>
-    )
+    return class ValueSetter extends React.Component<Props<S, T>, State<T>> {
+
+        state = {
+            ...initialValue
+        }
+
+        init = (config: S) => {
+            this.setState({
+                ...initializer(config)
+            });
+        }
+
+        render() {
+            return (
+                <React.Fragment>
+                    {this.props.children({
+                        init: this.init,
+                        ...this.state
+                    })}
+                </React.Fragment>
+            )
+        }
+    }
 }
