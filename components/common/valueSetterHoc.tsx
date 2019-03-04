9891a6a0b370f8879e29393ecb7d688475e25052
Ben Wilcox <reccanti@gmail.com>
2019-03-03 20:43:27 -0500

    Create withValueSetter HOC

--- components/common/valueSetterHoc.tsx
+++ components/common/valueSetterHoc.tsx
@@ -6,20 +6,21 @@
  */
 import * as React from 'react';
 
-type Props<S, T> = {
-    children: (values: T & { init: (config: S) => void }) => React.ReactNode
+type InitFunction<Config> = (config: Config) => void;
+type Props<Config, Value> = {
+    children: (values: Value & { init: InitFunction<Config> }) => React.ReactNode
 }
-type State<T> = T
+type State<Value> = Value
 
-export default function makeValueSetter<S, T>(initializer: (config: S) => T, initialValue: T): React.ComponentClass<Props<S, T>, State<T>> {
+export default function makeValueSetter<Config, Value>(initializer: (config: Config) => Value, initialValue: Value): React.ComponentClass<Props<Config, Value>, State<Value>> {
 
-    return class ValueSetter extends React.Component<Props<S, T>, State<T>> {
+    return class ValueSetter extends React.Component<Props<Config, Value>, State<Value>> {
 
         state = {
             ...initialValue
         }
 
-        init = (config: S) => {
+        init = (config: Config) => {
             this.setState({
                 ...initializer(config)
             });
@@ -37,3 +38,29 @@ export default function makeValueSetter<S, T>(initializer: (config: S) => T, ini
         }
     }
 }
+
+/**
+ * A higher-order component which uses `makeValueSetter` to create an
+ * entire structure using just the function
+ * 
+ * @param initializer a function which creates the values that will be passed from a given configuration
+ * @param initialValue the default values that will be used before the value is initialized by the initializer
+ * @param mapValueToProps a function which maps those values to a set of props
+ */
+export function withValueSetter<Config, Value, Props>(
+    initializer: (config: Config) => Value,
+    initialValue: Value,
+    mapValueToProps: (value: Value & { init: InitFunction<Config> }) => Props
+) {
+    const ValueSetter = makeValueSetter(initializer, initialValue)
+    return (Component: React.ComponentType<Props>) => (props: Props) => (
+        <ValueSetter>
+            {value => {
+                const valueProps = mapValueToProps(value);
+                return (
+                    <Component {...props} {...valueProps} />
+                )
+            }}
+        </ValueSetter>
+    )
+} 
