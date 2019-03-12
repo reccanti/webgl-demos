a6ed9a4128944cffc5d6b0f6356e59ba81323c8b
Ben Wilcox <reccanti@gmail.com>
2019-03-11 20:51:20 -0400

    Add test for initializer with Config
    Adds a test to show that the value can be set by calling an initializer
    with a config

--- components/common/__test__/valueContext.test.tsx
+++ components/common/__test__/valueContext.test.tsx
@@ -65,4 +65,51 @@ describe("ValueContext", () => {
 
         expect(wrapper.find(MyComponent).props()).toEqual({ foo: "bar" })
     })
+
+    test("The ValueContext created can be updated by an `initializer` function with parameters", () => {
+
+        // 1. create the context
+        type Config = string;
+        type Value = {
+            foo: Config
+        };
+        function initializer(config: Config): Value {
+            return {
+                foo: config
+            }
+        }
+        const {
+            withValueContextProvider,
+            withValueContextContainer,
+            withValueContextInitializer
+        } = makeValueContext(initializer, { foo: "baz" });
+
+        // 2. initialize the components
+        class Initializer extends React.Component<{ initialize?: (config: Config) => void }> {
+            handleInitialize = () => {
+                if (this.props.initialize) {
+                    this.props.initialize("bar");
+                }
+            }
+            render() {
+                return <React.Fragment />
+            }
+        }
+        const WrappedInitializer = withValueContextInitializer(Initializer);
+        const MyComponent = () => (
+            <React.Fragment>
+                <WrappedInitializer />
+            </React.Fragment>
+        )
+        const WrappedComponent = withValueContextProvider(withValueContextContainer(MyComponent));
+        const wrapper = mount(<WrappedComponent />)
+
+        // 3. simulate the Initializer's handleInitialize() function being called
+        const InitializerInstance = wrapper.find(Initializer).instance() as Initializer;
+        InitializerInstance.handleInitialize();
+        wrapper.update();
+
+        // 4. check to see if the value was updated
+        expect(wrapper.find(MyComponent).props()).toEqual({ foo: "bar" })
+    })
 })
